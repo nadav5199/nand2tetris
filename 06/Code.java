2353c6f7cb0c0f303d0229a98f1e2dfa9ed528c1
Nadav passal <nadav.passal@post.runi.ac.il>
2024-02-10 14:25:12 +0200

    last fixes

--- 06/Code.java
+++ 06/Code.java
@@ -2,64 +2,106 @@ public class Code {
     public static String dest(String dest) {
         if (dest == null) {
             return "000";
+        } else if (dest.equals("M")) {
+            return "001";
+        } else if (dest.equals("D")) {
+            return "010";
+        } else if (dest.equals("DM") || dest.equals("MD")) {
+            return "011";
+        } else if (dest.equals("A")) {
+            return "100";
+        } else if (dest.equals("AM")) {
+            return "101";
+        } else if (dest.equals("AD")) {
+            return "110";
+        } else if (dest.equals("ADM") || dest.equals("AMD")) {
+            return "111";
+        } else {
+            return "000";
         }
-        return switch (dest) {
-            case "M" -> "001";
-            case "D" -> "010";
-            case "DM","MD" -> "011";
-            case "A" -> "100";
-            case "AM" -> "101";
-            case "AD" -> "110";
-            case "ADM","AMD" -> "111";
-            default -> "000";
-        };
     }
+
     public static String comp(String comp) {
-        return switch (comp) {
-            case "0" -> "0101010";
-            case "1" -> "0111111";
-            case "-1" -> "0111010";
-            case "D" -> "0001100";
-            case "A" -> "0110000";
-            case "!D" -> "0001101";
-            case "!A" -> "0110001";
-            case "-D" -> "0001111";
-            case "-A" -> "0110011";
-            case "D+1" -> "0011111";
-            case "A+1" -> "0110111";
-            case "D-1" -> "0001110";
-            case "A-1" -> "0110010";
-            case "D+A" -> "0000010";
-            case "D-A" -> "0010011";
-            case "A-D" -> "0000111";
-            case "D&A" -> "0000000";
-            case "D|A" -> "0010101";
-            case "M" -> "1110000";
-            case "!M" -> "1110001";
-            case "-M" -> "1110011";
-            case "M+1" -> "1110111";
-            case "M-1" -> "1110010";
-            case "D+M" -> "1000010";
-            case "D-M" -> "1010011";
-            case "M-D" -> "1000111";
-            case "D&M" -> "1000000";
-            case "D|M" -> "1010101";
-            default -> "0000000";
-        };
+        if (comp.equals("0")) {
+            return "0101010";
+        } else if (comp.equals("1")) {
+            return "0111111";
+        } else if (comp.equals("-1")) {
+            return "0111010";
+        } else if (comp.equals("D")) {
+            return "0001100";
+        } else if (comp.equals("A")) {
+            return "0110000";
+        } else if (comp.equals("!D")) {
+            return "0001101";
+        } else if (comp.equals("!A")) {
+            return "0110001";
+        } else if (comp.equals("-D")) {
+            return "0001111";
+        } else if (comp.equals("-A")) {
+            return "0110011";
+        } else if (comp.equals("D+1")) {
+            return "0011111";
+        } else if (comp.equals("A+1")) {
+            return "0110111";
+        } else if (comp.equals("D-1")) {
+            return "0001110";
+        } else if (comp.equals("A-1")) {
+            return "0110010";
+        } else if (comp.equals("D+A")) {
+            return "0000010";
+        } else if (comp.equals("D-A")) {
+            return "0010011";
+        } else if (comp.equals("A-D")) {
+            return "0000111";
+        } else if (comp.equals("D&A")) {
+            return "0000000";
+        } else if (comp.equals("D|A")) {
+            return "0010101";
+        } else if (comp.equals("M")) {
+            return "1110000";
+        } else if (comp.equals("!M")) {
+            return "1110001";
+        } else if (comp.equals("-M")) {
+            return "1110011";
+        } else if (comp.equals("M+1")) {
+            return "1110111";
+        } else if (comp.equals("M-1")) {
+            return "1110010";
+        } else if (comp.equals("D+M")) {
+            return "1000010";
+        } else if (comp.equals("D-M")) {
+            return "1010011";
+        } else if (comp.equals("M-D")) {
+            return "1000111";
+        } else if (comp.equals("D&M")) {
+            return "1000000";
+        } else if (comp.equals("D|M")) {
+            return "1010101";
+        } else {
+            return "0000000";
+        }
     }
+
     public static String jump(String jump) {
         if (jump == null) {
             return "000";
+        } else if (jump.equals("JGT")) {
+            return "001";
+        } else if (jump.equals("JEQ")) {
+            return "010";
+        } else if (jump.equals("JGE")) {
+            return "011";
+        } else if (jump.equals("JLT")) {
+            return "100";
+        } else if (jump.equals("JNE")) {
+            return "101";
+        } else if (jump.equals("JLE")) {
+            return "110";
+        } else if (jump.equals("JMP")) {
+            return "111";
+        } else {
+            return "000";
         }
-        return switch (jump) {
-            case "JGT" -> "001";
-            case "JEQ" -> "010";
-            case "JGE" -> "011";
-            case "JLT" -> "100";
-            case "JNE" -> "101";
-            case "JLE" -> "110";
-            case "JMP" -> "111";
-            default -> "000";
-        };
     }
 }
--- 06/Main.java
+++ 06/Main.java
@@ -1,16 +1,45 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     static int countVar = 0;
     static int countNew = 16;
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        final Path inputPath = Paths.get(args[0]);
+        // Check if the input is a directory or a single file
+        if (Files.isDirectory(inputPath)) {
+            // Process all .asm files in the directory
+            try (Stream<Path> paths = Files.list(inputPath)) {
+                for (Path p : paths.toList()) {
+                    if (p.toString().endsWith(".asm")) {
+                        process(p.toFile());
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("An error occurred while listing the files: " + e.getMessage());
+            }
+        } else if (inputPath.toString().endsWith(".asm")) {
+            // If the input is a single .asm file, process it
+            process(inputPath.toFile());
+        } else {
+            System.out.println("The specified path is not a directory or an .asm file.");
+        }
+    }
+    public static void process(File fileName) {
         try {
-            Parser parser1 = new Parser(args[0]);
-            Parser parser2 = new Parser(args[0]);
+            String name = fileName.toPath().toString();
+            Parser parser1 = new Parser(name);
+            Parser parser2 = new Parser(name);
             SymbolTable symbolTable = new SymbolTable();
             init(symbolTable);
             // create new hack file.
-            String path = args[0].replaceAll(".asm", ".hack");
+            String path = name.replaceAll(".asm", ".hack");
             File file = new File(path);
             if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
@@ -62,7 +91,6 @@ public class Main {
         }
 
     }
-
     public static void init(SymbolTable symbolTable) {
         for (int i = 0; i < 16; i++) {
             symbolTable.addEntry("R" + i, i);
